67102f353b072a39920f62e7e8ddc713a7ffbc0a
pyerfox <wooya2k@naver.com>
2023-12-30 11:48:18 +0900

    evil

--- config.el
+++ config.el
@@ -67,3 +67,19 @@
 
 ;; Don't install anything. Defer execution of BODY
 ;; (elpaca nil (message "deferred"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (evil-mode))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (evil-collection-init))
+
+(use-package evil-tutor)
--- init.el
+++ init.el
@@ -1,3 +1,39 @@
+
+(setq gc-cons-threshold (* 1024 1024 100))
+(setq read-process-output-max (* 1024 1024))
+
+(global-unset-key (kbd "S-SPC"))
+
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+(setq select-enable-clipboard nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq inhibit-startup-message t)
+(setq visual-bell t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+  (dolist (mode '(org-mode-hook
+                  term-mode-hook
+                  shell-mode-hook
+                  eshell-mode-hook))
+(add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(set-face-attribute 'default nil
+		    :font "Hack"
+		    :weight 'normal
+		    :height 100)
+(set-fontset-font "fontset-default" 'hangul '("D2Coding" . "unicode-bmp"))
+
+(load-theme 'tsdh-dark t)
+
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
